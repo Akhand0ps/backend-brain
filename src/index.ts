d48737151f1c361759f4a1c62845b8f72ffe2145
Akhand0ps <akhandps08@gmail.com>
2025-07-06 17:00:13 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
