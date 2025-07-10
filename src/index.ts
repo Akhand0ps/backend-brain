7d48c98af1c90c864f9f37ce73a028a97e03c0e3
Akhand0ps <akhandps08@gmail.com>
2025-07-10 21:58:06 +0530

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://yourbrain.vercel.app",
   "https://vercel.com/akhand0ps-projects/brain-frontend-ha7o/Gy8TCo5vinTcmZjkwhzRRjDfmD8E",
-  "https://brain-frontend-ha7o-git-main-akhand0ps-projects.vercel.app/"
+  "https://brain-frontend-ha7o-git-main-akhand0ps-projects.vercel.app/",
+  "https://brain-frontend-gilt.vercel.app/"
 ];
 
 
