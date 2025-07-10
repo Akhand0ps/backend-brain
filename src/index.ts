f16cb8051d851e31edb2afeb8c8ddc0ced687a3e
Akhand0ps <akhandps08@gmail.com>
2025-07-10 23:20:36 +0530

    fix error

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://yourbrain.vercel.app",
-  "https://vercel.com/akhand0ps-projects/brain-frontend-ha7o/Gy8TCo5vinTcmZjkwhzRRjDfmD8E",
-  "https://brain-frontend-ha7o-git-main-akhand0ps-projects.vercel.app",
-  "https://brain-frontend-gilt.vercel.app"
+  "https://brain-frontend-seven.vercel.app"
 ];
 
 
